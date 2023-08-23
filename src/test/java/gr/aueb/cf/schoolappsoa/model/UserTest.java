aef6cd3b1da92f2fbe8fcad787f274bc99424614
sakis <th.chousiadas@gmail.com>
2023-08-23 17:42:31 +0300

    Adding JavaDocs in UserTest class

--- src/test/java/gr/aueb/cf/schoolappsoa/model/UserTest.java
+++ src/test/java/gr/aueb/cf/schoolappsoa/model/UserTest.java
@@ -4,7 +4,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * This unit test class tests the
+ * {@link User} model class where
+ * User Entity is declared.
+ *
+ * @author Thanasis Chousiadas
+ */
 public class UserTest {
+
+    /**
+     * This method tests the getters and setters
+     * for each attribute of the {@link User} class.
+     */
     @Test
     void gettersSetters() {
         User user = new User();
@@ -17,6 +29,10 @@ public class UserTest {
         assertEquals(user.getPassword(), "123456abc");
     }
 
+    /**
+     * This class tests the constructor of
+     * {@link User} class.
+     */
     @Test
     void overloadedConstructor() {
         User user = new User(1L, "user-01", "123456abc");
